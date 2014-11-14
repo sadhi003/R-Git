08fd9babc0a645a01c6c349a024971f3de2894f3
Shankar Adhikari <adhi.shankar3@gmail.com>
2014-11-13 19:53:39 -0500

    11

---
+++ new-Project.R
@@ -0,0 +1,18 @@
+
+
+getwd()
+#dir()
+shankar <- read.csv("~/R_programming/Obes.cvs", True,",")
+class(shankar)
+head(shankar)
+# how to run HTML package and use data
+require(XML)
+items <- readHTMLTable("link for site", which -1) # which table I have to read
+head(items)
+setwd("~/R_programming/   ")
+dir()
+b <- read.table(file="test.csv", header=T, sep=";")
+dim(b)
+head(b)
+tail(b)
+dir()
